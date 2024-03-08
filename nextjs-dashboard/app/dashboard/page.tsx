3e50d8384eb096aeb4fc1114f315332b97f40afa
neuche <igna.eg@gmail.com>
2024-03-08 16:46:05 -0300

    Added skeletons components to different suspense cycles component fetchs

--- nextjs-dashboard/app/dashboard/page.tsx
+++
@@ -1,5 +0,0 @@
-import { lusitana } from "../ui/fonts";
-
-export default function Page() {
-    return <p className={`${ lusitana.className }text-xl text-gray-800 md:text-3xl md:leading-normal`}> Dashboard Page</p>;
-  }
